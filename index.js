25a6fbdd71d86cc4bbccd88afa077adb26f52d9f
Sebastian Vega <seba.kc@gmail.com>
2024-02-06 22:33:36 -0300

    feature: +temas command

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const ircClient = require('node-irc')
-const cheerio = require('cheerio')
 const axios = require('axios')
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache()
 
-const { CHANNEL, USERS, BOT_NAME, HOST, PORT, BOT_NICK, RADIO_LINK } = process.env
+const { CHANNEL, USERS, BOT_NAME, HOST, PORT, BOT_NICK, RADIO_LINK, API, TEMAS } = process.env
 var client = new ircClient(HOST, PORT, BOT_NICK, BOT_NAME);
 
 client.on('ready', function () {
@@ -15,11 +16,27 @@ client.on('PRIVMSG', function (data) {
 })
 
 client.on('CHANMSG', function (data) {
-  console.log('chan', data)
   if (USERS.includes(data.sender)) {
     if (data.message === '+radio') {
       client.say(CHANNEL, RADIO_LINK);
     }
+    if (data.message === '+temas') {
+      myCache.set('scream', true)
+      setTimeout(() => {
+        myCache.set('scream', false)
+      }, 1000 * 60 * parseInt(TEMAS))
+    }
   }
 })
+
+setInterval(() => {
+  axios.post(API)
+    .then(res => {
+      const prev = myCache.get("current")
+      if (prev !== res.data.title && myCache.get('scream')) {
+        client.say(CHANNEL, `${res.data.title} 🎶`);
+        success = myCache.set("current", res.data.title);
+      }
+    })
+}, 10000)
 client.connect();
