0fbc4abad6ce7ae80d322cb218524c37c5423997
Sebastian Vega <seba.kc@gmail.com>
2024-02-07 16:05:33 -0300

    feature: temas down

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 require('dotenv').config()
-const ircClient = require('node-irc')
 var irc = require('irc')
 const axios = require('axios')
 const NodeCache = require( "node-cache" );
 const myCache = new NodeCache()
 
+const TIMEOUT = 185
+let speak = false
+let start = new Date()
+
 const { CHANNEL, USERS, BOT_NAME, HOST, PORT, BOT_NICK, RADIO_LINK, IG_LINK, API, TEMAS, PASSWORD } = process.env
-const params = [HOST, PORT, BOT_NICK, BOT_NAME]
-// PASSWORD && params.push(PASSWORD)
-// console.log(params)
-var client = new ircClient(...params);
 var client = new irc.Client(HOST, BOT_NICK, {
   channels: [CHANNEL],
   userName: BOT_NICK,
   password: PASSWORD,
   realName: BOT_NAME,
-});
-
-let speak = false
-let start = new Date()
-setTimeout(() => {
-  speak = true
-}, 1000 * 200)
+})
 
 const say = (channel, message) => {
   if(!speak) {
     const now = new Date()
-    console.log(`w8 ${200 - ((now - start)/1000)} seconds more`)
+    console.log(`w8 ${TIMEOUT - ((now - start)/1000)} seconds more`)
     return
   }
   try {
@@ -35,6 +28,31 @@ const say = (channel, message) => {
     console.log(error)
   }
 }
+
+// time to w8 to speak in the room
+setTimeout(() => {
+  speak = true
+  say(CHANNEL, 'bot online')
+}, 1000 * TIMEOUT)
+
+const temas = (sec = 10) => {
+  const tell = () => {
+    axios.post(API)
+      .then(res => {
+        const prev = myCache.get("current")
+        if (prev !== res.data.title && myCache.get('scream')) {
+          say(CHANNEL, `${res.data.title} (8)`);
+          success = myCache.set("current", res.data.title);
+        }
+      })
+  }
+  tell()
+  setInterval(() => {
+    tell()
+  }, 1000 * sec)
+}
+
+let temasInterval
 
 client.addListener(`message${CHANNEL}`, function (from, message) {
   if (USERS.includes(from)) {
@@ -49,19 +67,16 @@ client.addListener(`message${CHANNEL}`, function (from, message) {
     }
     if (message === '+temas') {
       myCache.set('scream', true)
+      temasInterval = temas()
       setTimeout(() => {
+        say(CHANNEL, 'temas off');
+        clearInterval(temasInterval)
         myCache.set('scream', false)
       }, 1000 * 60 * parseInt(TEMAS))
     }
+    if (message === '+temas down') {
+      clearInterval(temasInterval)
+      myCache.set('scream', false)
+    }
   }
 });
-setInterval(() => {
-  axios.post(API)
-  .then(res => {
-      const prev = myCache.get("current")
-      if (prev !== res.data.title && myCache.get('scream')) {
-        say(CHANNEL, `${res.data.title} (8)`);
-        success = myCache.set("current", res.data.title);
-      }
-    })
-}, 10000)
