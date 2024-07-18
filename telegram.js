678e9def3ca04d0d7f899ba308779e73a830f3b3
Sebastian Vega <seba.kc@gmail.com>
2024-07-18 15:56:31 -0400

    fix

--- telegram.js
+++ telegram.js
@@ -1,11 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 const { TELEGRAM_BOT_TOKEN } = process.env
-const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, {polling: true});
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
+let bot
+if(TELEGRAM_BOT_TOKEN) {
+  bot = new TelegramBot(TELEGRAM_BOT_TOKEN, {polling: true});
+  bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+  
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+  });
+}
 module.exports = bot
